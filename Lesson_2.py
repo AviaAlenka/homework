289acd8e03b9a91bf607c6dd5b23bc6ff280b9d3
AviaAlenka <LenaSamara@list.ru>
2024-09-28 21:06:56 +0400

    Д/з №2

---
+++ Lesson_2.py
@@ -0,0 +1,6 @@
+vsego_zadach = "12"
+vremya = "1.5"
+kurs = "Python"
+srednee_vremya = float(vremya) / float(vsego_zadach)
+print("Курс:",kurs+",", "всего задач:", vsego_zadach+",", "затрачено часов:", vremya+",", "среднее время выполнения", srednee_vremya, "часа"+".")
+
