3922027efd37f297988c6d5b6e99ef0cd1d2b87e
AviaAlenka <LenaSamara@list.ru>
2024-10-01 22:13:12 +0400

    1-й блок задание 4

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Напишите что-нибудь: ")
+print("количество символов:", len(my_string))
+print("В верхнем регистре:", my_string .upper())
+print("В нижнем регистре:", my_string .lower())
+print("Без пробелов:", my_string .replace(' ', ''))
+print("Первый символ:", my_string [0])
+print("Последний символ:", my_string [-1])
