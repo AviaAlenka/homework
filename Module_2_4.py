136443b8c9598af2ed11fec1c9ca99bf03f2656a
AviaAlenka <LenaSamara@list.ru>
2024-10-04 12:46:44 +0400

    проверка списка на простоту чисел 2

--- Module_2_4.py
+++ Module_2_4.py
@@ -1,15 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-k = 0
 for i in range(len(numbers)):
     for j in range(2, numbers[i]):
         if (numbers[i] % j == 0):
-            k = k+1
-    if (k <= 0):
-        primes.append(numbers[i])
-    else:
-        not_primes.append(numbers[i])
-print(primes)
-print(not_primes)
+            not_primes.append(numbers[i])
+primes = set(numbers) - set(not_primes)
+print(str(primes))
+print(set(not_primes))
 
