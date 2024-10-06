623013ef7ba26429d59da3db4be16d793465e546
AviaAlenka <LenaSamara@list.ru>
2024-10-07 03:32:37 +0400

    Создание матрицы с одинаковыми значениями

--- module_2_5.py
+++ module_2_5.py
@@ -1,11 +1,11 @@
-n = int(input("количество строк: "))
-m = int(input("количество столбцов: "))
+n = int(input("Количество строк: "))
+m = int(input("Количество столбцов: "))
 value = int(input("значения: "))
-stroki = []
-def get_matrix(n,m,value):
+def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         matrix.insert(i, [value]*int(m))
     return matrix
 matrix = get_matrix(n, m, value)
+print("Получим матрицу", str(n) + "х" + str(m) + ":")
 print(matrix)
