e38587eed5667cd460330c7b637acad57ce19129
AviaAlenka <LenaSamara@list.ru>
2024-10-07 11:48:43 +0400

    выбор простых и непростых чисел из списка

--- Module_2_4.py
+++ Module_2_4.py
@@ -1,11 +1,21 @@
+print("Дан список чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]")
+print("Задача: выбрать из этого списка сначала простые числа, "
+      "а затем выбрать не простые.")
+print("Единица не является ни простым, ни сложным числом - "
+      "её необходимо исключить.")
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-for i in range(len(numbers)):
-    for j in range(2, numbers[i]):
-        if (numbers[i] % j == 0):
-            not_primes.append(numbers[i])
-primes = set(numbers) - set(not_primes)
-print(str(primes))
-print(set(not_primes))
-
+numbers_bez_1 = list(filter(lambda x: x != 1, numbers))
+for i in range(len(numbers_bez_1)):
+    for j in range(2, numbers_bez_1[i]):
+        if (numbers_bez_1[i] % j == 0):
+            not_primes.append(numbers_bez_1[i])
+        else:
+            primes.append(numbers_bez_1[i])
+primes = set(numbers_bez_1) - set(not_primes)
+print("Простые числа:", list(primes))
+print("Не простые числа:", list(set(not_primes)))
+# Работает с любым количеством чисел в списке и с любым положением единицы в списке.
+# Проверяла :-)
