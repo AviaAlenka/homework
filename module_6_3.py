6280c41239b1672a5dd1a010d82452385be3caa2
AviaAlenka <LenaSamara@list.ru>
2024-11-12 20:43:45 +0400

    Множественное наследование (утконос)

--- module_6_3.py
+++ module_6_3.py
@@ -5,7 +5,7 @@ class Animal:
     sound = None
     _DEGREE_OF_DANGER = 0
 
-    def __init__(self, _cords, speed):
+    def __init__(self, _cords, speed=0):
         self._cords = _cords
         self.speed = speed
 
@@ -13,19 +13,23 @@ class Animal:
     speed = int()
 
     def move(self, dx, dy, dz):
-        pass
+        if dz < 0:
+            print("It's too deep, i can't dive :(")
+            # print(Animal._cords) # проверка координат
+        else:
+            Animal._cords = [(x + y) for x, y in zip(Animal._cords, (dx, dy, dz))]
 
     def get_cords(self):
-        pass
+        speed = int(Animal.speed) + int(db.speed)
+        Animal._cords = (Animal._cords[0] * speed, Animal._cords[1] * speed, Animal._cords[2] * speed)
+        # print(Animal._cords)
+        print(f"X: {Animal._cords[0]} Y: {Animal._cords[1]}, Z: {Animal._cords[2]}")
 
     def attack(self):
         if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
         elif self._DEGREE_OF_DANGER > 5:
             print("Be careful, i'm attacking you 0_0")
-
-    def about(self):
-        print("Ку-ку")
 
 class Bird(Animal):
     beak = True
@@ -44,7 +48,9 @@ class AquaticAnimal(Animal):
         super().__init__(_cords, speed)
 
     def dive_in(self, dz):
-        pass
+        # print(f"Класс водоплавающие: {Animal._cords}") # проверка координат
+        print(f"X: {Animal._cords[0]} Y: {Animal._cords[1]}, "
+              f"Z: {int((Animal._cords[2]  - (abs(dz) * db.speed) / 2))}")
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
@@ -52,27 +58,20 @@ class PoisonousAnimal(Animal):
     def __init__(self, _cords, speed):
         super().__init__(_cords, speed)
 
-class Duckbill(Bird, AquaticAnimal, PoisonousAnimal, Animal):
+class Duckbill(PoisonousAnimal,Bird, AquaticAnimal,  Animal):
     sound = "Click-click-click"
     def __init__(self, _cords, speed):
         super().__init__(_cords, speed)
-        super().about()
     def speak(self):
-        print(self.sound)
+        print(f"Животное издаёт звуки: {self.sound}")
 
-    
-
-db = Duckbill(10, 10)
-
+db = Duckbill(0, 10)
 print(f"Животное живо: {db.live}")
 print(f"У животного есть клюв: {db.beak}")
-
 db.speak()
 db.attack()
-
 db.move(1, 2, 3)
 db.get_cords()
 db.dive_in(6)
-db.get_cords()
-
 db.lay_eggs()
+# print(Duckbill.mro()) # проверка порядка наследования
