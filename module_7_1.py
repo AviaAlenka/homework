35ea0b54ca1c037c05bd7e91c33fd3fec81a4db1
AviaAlenka <LenaSamara@list.ru>
2024-11-19 23:09:26 +0400

    Чтение-запись в файл

--- module_7_1.py
+++ module_7_1.py
@@ -2,42 +2,50 @@ from pprint import pprint
 
 class Product:
 
-    # def __new__(cls, *args):
-    #     return super().__new__(cls)
+    def __new__(cls, *args):
+        return super().__new__(cls)
 
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
         self.category = category
 
-    products = []
+    product = ""
     def __str__(self):
-        self.products.append(f"{str(self.name)}, {float(self.weight)}, {str(self.category)}")
-        return str(self.products)
+        self.product = f"{str(self.name)}, {float(self.weight)}, {str(self.category)}"
+        return str(self.product)
 
-class Shop:
+class Shop(Product):
     __file_name = 'products.txt'
+
+    def __init__(self, name, weight, category):
+        super().__init__(name, weight, category)
 
     def get_products(self):
         file = open(self.__file_name, 'r')
         pprint(file.read())
         file.close()
-        # return f'{pprint(file.read())}'
+        # return self.get_products
+
 
     def add(self, *products):
-        products = Product.products
-
         file = open(self.__file_name, 'a')
-        file.write(f"\n {str(products)}")
-        file.close()
-        return str(products)
+        for item in products:
+            # if self is str(item) or self == str(item):
+            if item in products:
+                print(f"{str(item)} уже есть в магазине")
+            else:
+                file.writelines(f"{str(item)}\n")
 
-s1 = Shop()
+        file.close()
+        return self.get_products
+
+s1 = Shop('', 0, '')
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p1, p2, p3) # __str__
+print(p2) # __str__
 s1.add(p1, p2, p3)
 print(s1.get_products())
-print(s1.add())
+
