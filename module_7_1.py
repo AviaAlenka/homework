730ec3e01acd0cf4d31ea4438a5a02feaa703e02
AviaAlenka <LenaSamara@list.ru>
2024-11-19 13:53:15 +0400

    Запись в файл

--- module_7_1.py
+++ module_7_1.py
@@ -1,14 +1,19 @@
 from pprint import pprint
 
 class Product:
+
+    # def __new__(cls, *args):
+    #     return super().__new__(cls)
+
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
         self.category = category
 
+    products = []
     def __str__(self):
-        # print(f"Товар: {str(self.name)}, {float(self.weight)}, {str(self.category)}")
-        return f"{str(self.name)}, {float(self.weight)}, {str(self.category)}"
+        self.products.append(f"{str(self.name)}, {float(self.weight)}, {str(self.category)}")
+        return str(self.products)
 
 class Shop:
     __file_name = 'products.txt'
@@ -17,15 +22,15 @@ class Shop:
         file = open(self.__file_name, 'r')
         pprint(file.read())
         file.close()
-        return str(file)
+        # return f'{pprint(file.read())}'
 
     def add(self, *products):
+        products = Product.products
+
         file = open(self.__file_name, 'a')
-        file.writelines(f"\n {products}")
+        file.write(f"\n {str(products)}")
         file.close()
-        return str(file)
-
-
+        return str(products)
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
@@ -35,3 +40,4 @@ p3 = Product('Potato', 5.5, 'Vegetables')
 print(p1, p2, p3) # __str__
 s1.add(p1, p2, p3)
 print(s1.get_products())
+print(s1.add())
