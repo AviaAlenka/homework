0b33eff6a8e2f34aef7c490f92c00d6896404cf3
Elena <LenaSamara@list.ru>
2025-01-20 07:47:38 +0400

    Update Bot.py
    Без ключа

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor,types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "8100419843:AAHoQivMyXedI3BxgqlQH2lKKnY_aiXx2SM"
+api = ""
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
